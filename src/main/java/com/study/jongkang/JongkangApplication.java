11a0a27bb5d6571fde4f5b9c65a1dcb8008eaf0f
hjj4060 <rkdgustn77@naver.com>
2021-12-01 21:25:02 +0900

    Initial commit

---
+++ src/main/java/com/study/jongkang/JongkangApplication.java
@@ -0,0 +1,13 @@
+package com.study.jongkang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JongkangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JongkangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/jongkang/JongkangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jongkang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JongkangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
